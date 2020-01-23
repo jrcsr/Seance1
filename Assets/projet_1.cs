0fbc0930fd1f7d2769c92a4983f60a03b58af0df
jrcsr <56159912+jrcsr@users.noreply.github.com>
2020-01-23 15:18:33 +0100

    Initial commit

---
+++ Assets/projet_1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projet_1 : MonoBehaviour
+{
+    public int nbrObjets = 5;
+    public int radius = 2;
+    public GameObject prefab1;
+    private GameObject[] a;
+    float x;
+
+    void Start()
+    {
+       
+        Instantiate(prefab1, new Vector3 (0,0,0), Quaternion.identity);
+        
+    }
+
+    void Update()
+    {
+        DeleteCube();
+        x += Time.deltaTime * 50;
+        for(var i = 0; i<nbrObjets; i++)
+        {
+            var angle = i * Mathf.PI * 2 / nbrObjets;
+            var pos = new Vector3 (Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+            Instantiate(prefab1, pos, Quaternion.Euler(x,x,x));
+        }
+    }
+
+    void DeleteCube()
+    {
+        a = GameObject.FindGameObjectsWithTag("Cube");
+        foreach(GameObject o in a)
+        {
+            Destroy(o);
+        }
+    }
+}
